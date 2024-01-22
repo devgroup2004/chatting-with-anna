fe674421c45d98c14b5160760e9e8e57aab05105
DevFlashPlus <devflash360@gmail.com>
2024-01-22 23:08:37 +0900

    chatting-with-anna

--- src/pages/product/BuyProduct.js
+++ src/pages/product/BuyProduct.js
@@ -1,38 +1,43 @@
+import  "./style.scss";
+
 const BuyProduct = () => {
     return (
         <>
-            <div className="rounded-lg overflow-hidden shadow-md border border-gray-300 m-4">
-                <div className="bg-gray-400 p-2 flex justify-start items-center">
-                    <p className="pl-10 pr-10 mr-10 font-bold text-white text-2xl">Your Cart</p>
+            <div className="my-card m-4">
+                <div className="my-card-header">
+                    <p className="ml-10 mr-10">Your Cart</p>
                 </div>
-                <div class="bg-white p-4">
-                    Some comment
+                <div className="my-card-body">
+                    <div className="border border-gray-300 col-span-1"> image </div>
+                    <div className="border border-gray-300 col-span-3">
+                        <p className="text-2xl">Vinut Sparkling Watermelon Juice, 6 x 11 Fl oz Cans, Non-GMO, No added Sugar, artificial preservatives, flavors, or colors, Rich in in vitamins A, C, and B6, antioxidants</p>
+                    </div>
                 </div>
             </div>
-            <div className="rounded-lg overflow-hidden shadow-md border border-gray-300 m-4">
-                <div className="bg-gray-400 p-2 flex justify-start items-center">
-                    <p className="pl-10 pr-10 font-bold text-white text-2xl">Order Summary</p>
+            <div className="my-card m-4">
+                <div className="my-card-header">
+                    <p className="ml-10 mr-10">Order Summary</p>
                 </div>
-                <div class="bg-white p-4">
+                <div className="bg-white p-4">
                     Some comment
                 </div>
             </div>
             <div className="m-4 grid grid-cols-3 gap-4">
 
-                <div className="rounded-lg overflow-hidden shadow-md border border-gray-300 col-span-2">
-                    <div className="bg-gray-400 p-2 flex justify-start items-center">
-                        <p className="pl-10 pr-10 font-bold text-white text-2xl">Your Detail</p>
+                <div className="my-card col-span-2">
+                    <div className="my-card-header">
+                        <p className="pl-10 pr-10">Your Detail</p>
                     </div>
-                    <div class="bg-white p-4">
+                    <div className="bg-white p-4">
                         Some comment
                     </div>
                 </div>
                     
-                <div className="rounded-lg overflow-hidden shadow-md border border-gray-300 col-span-1">
-                    <div className="bg-gray-400 p-2 flex justify-start items-center">
-                        <p className="pl-10 pr-10 font-bold text-white text-2xl">Payment Method</p>
+                <div className="my-card col-span-1">
+                    <div className="my-card-header">
+                        <p className="pl-10 pr-10">Payment Method</p>
                     </div>
-                    <div class="bg-white p-4">
+                    <div className="bg-white p-4">
                         Some comment
                     </div>
                 </div>
