8fed17c36959ed0b2f501c933e4c8e46764f484a
DevFlashPlus <devflash360@gmail.com>
2024-01-22 21:17:57 +0900

    chatting-with-anna

--- src/pages/product/BuyProduct.js
+++ src/pages/product/BuyProduct.js
@@ -1,7 +1,14 @@
 const BuyProduct = () => {
     return (
         <>
-            <div>Your Cart</div>
+            <div class="rounded-lg overflow-hidden shadow-md border border-gray-300 m-4">
+                <div class="bg-gray-400 p-4 flex justify-start items-center p-4 pl-10">
+                    Your Cart
+                </div>
+                <div class="bg-white p-4">
+                    Some comment
+                </div>
+            </div>
             <div>Order Summary</div>
             <div>Your Detail</div>
             <div>Payment Method</div>
